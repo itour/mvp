2dfd1802d84ce51ecc5ba5196cd86168d7b136ce
itour <wti-itour@hotmail.com>
2018-04-04 10:59:10 +0800

    modify maven.compiler.encoding to UTF-8

--- src/main/java/com/icss/mvp/Constants.java
+++ src/main/java/com/icss/mvp/Constants.java
@@ -1,85 +1,78 @@
-﻿package com.icss.mvp;
+package com.icss.mvp;
 
 /**
- * 
  * <pre>
  * <b>描述：常量类</b>
  * <b>任务编号：</b>
- * <b>作者：张鹏飞</b> 
+ * <b>作者：张鹏飞</b>
  * <b>创建日期： 2017年5月17日 下午2:37:43</b>
  * </pre>
  */
-public class Constants
-{
-	/**
-	 * session存储用户信息KEY
-	 */
-	public final static String USER_KEY = "USERINFO";
+public class Constants {
+    /**
+     * session存储用户信息KEY
+     */
+    public final static String USER_KEY = "USERINFO";
 
-	public final static String STATUS = "STATUS";
+    public final static String STATUS = "STATUS";
 
-	public final static String FAILED = "FAILED";
+    public final static String FAILED = "FAILED";
 
-	public final static String MESSAGE = "MESSAGE";
+    public final static String MESSAGE = "MESSAGE";
 
-	public final static String SUCCESS = "SUCCESS";
+    public final static String SUCCESS = "SUCCESS";
 
-	public final static String TR5DI = "114";
+    public final static String TR5DI = "114";
 
-	public final static String TR6DI = "115";
+    public final static String TR6DI = "115";
 
-	public final static String TOTAL_DTS_NUM = "98";
+    public final static String TOTAL_DTS_NUM = "98";
 
-	public final static String CODE_ADD_NUM = "129";
+    public final static String CODE_ADD_NUM = "129";
 
-	public final static String ITERATION_DEFECT_DENSITY = "106";
+    public final static String ITERATION_DEFECT_DENSITY = "106";
 
-	public final static String DTS_NO_PASS_NUM = "110";
+    public final static String DTS_NO_PASS_NUM = "110";
 
-	public final static String LEAVE_DI = "107";
+    public final static String LEAVE_DI = "107";
 
-	public final static String RESOLUTION_RATE = "111";
-	public final static String STRUCT_TIME = "struct_time";
-	
-	public final static int CI_UADP_GARDING = 99;
-	public final static int CI_SOURCE_MONITOR = 103;
-	public final static int CI_LLT_COVERAGE = 135;
-	public final static int CI_AVG_STRCUT = 120;
-	public final static int CI_STRCUT_RATE = 118;
-	public final static int CI_CODE_CC = 100;
-       
+    public final static String RESOLUTION_RATE = "111";
+    public final static String STRUCT_TIME = "struct_time";
+
+    public final static int CI_UADP_GARDING = 99;
+    public final static int CI_SOURCE_MONITOR = 103;
+    public final static int CI_LLT_COVERAGE = 135;
+    public final static int CI_AVG_STRCUT = 120;
+    public final static int CI_STRCUT_RATE = 118;
+    public final static int CI_CODE_CC = 100;
+
     public final static String SVI_DEFECT_DENSITY = "112";
-    
-    public final static String CODE_DUPLICATION  = "102";
-    
-    public final static String STATIC_CHECK_WARING  = "101";
-    
-    public final static String MONTH_PRODUCT_RATE  = "83";
-    
-    public final static String PROJECT_E2E_PRODUCT_RATE  = "84";
-    
-    public final static String ITERATION_PRODUCT_RATE  = "85";
-    
-   
-    
-    
 
-	// DTS 相关
-	public final static String CANCEL = "Cancel";
-	public final static String CLOSE = "Close";
-	public final static String CLOSURE_AFTER_CORRECTION = "Closure After Correction";
-	public final static String CRITIAL = "Critial";
-	public final static String MAJOR = "Major";
-	public final static String MINOR = "Minor";
-	public final static String SUGGESTION = "Suggestion";
-	public final static String SYSTEM_VERIFICATION_TEST_SVT = "System Verification Test(SVT)";
-	public final static String BETA_TEST = "Beta Test";
-	public final static String GA = "GA";
-	public final static String LIFT_CYCLE = "Lift Cycle(Including ESP Maintenace)";
-	public final static String NETWORK_ENTRY_TEST = "network entry test";
-	public final static String TR5_GA = "TR5-GA";
-	public final static String SYSTEM_INTEGRATION_TEST_SIT = "System Integration Test(SIT)";
-    
-   
-    
+    public final static String CODE_DUPLICATION = "102";
+
+    public final static String STATIC_CHECK_WARING = "101";
+
+    public final static String MONTH_PRODUCT_RATE = "83";
+
+    public final static String PROJECT_E2E_PRODUCT_RATE = "84";
+
+    public final static String ITERATION_PRODUCT_RATE = "85";
+
+
+    // DTS 相关
+    public final static String CANCEL = "Cancel";
+    public final static String CLOSE = "Close";
+    public final static String CLOSURE_AFTER_CORRECTION = "Closure After Correction";
+    public final static String CRITIAL = "Critial";
+    public final static String MAJOR = "Major";
+    public final static String MINOR = "Minor";
+    public final static String SUGGESTION = "Suggestion";
+    public final static String SYSTEM_VERIFICATION_TEST_SVT = "System Verification Test(SVT)";
+    public final static String BETA_TEST = "Beta Test";
+    public final static String GA = "GA";
+    public final static String LIFT_CYCLE = "Lift Cycle(Including ESP Maintenace)";
+    public final static String NETWORK_ENTRY_TEST = "network entry test";
+    public final static String TR5_GA = "TR5-GA";
+    public final static String SYSTEM_INTEGRATION_TEST_SIT = "System Integration Test(SIT)";
+
 }
